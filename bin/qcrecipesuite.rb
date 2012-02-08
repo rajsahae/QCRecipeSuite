a3607c4b516361b704de96d917776e29da82a84d
Raj Sahae <rajsahae@gmail.com>
2012-02-08 13:37:44 -0800

    Added bin/qcrecipesuite.rb for exploratory testing

---
+++ bin/qcrecipesuite.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/env ruby
+$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+require 'qcrecipesuite'
+
+raise StandardError.new "You must specify two comparison files" if ARGV.empty?
+file1, file2 = ARGV[0], ARGV[1]
+[file1, file2].each do |file| 
+  raise ArgumentError.new "File: #{file} doesn't exist" unless File.exists?(file)
+end
+analyzer = QCRecipeSuite::Analyzer.new(STDOUT)
+File.open(file1, 'r') do |first|
+  File.open(file2, 'r') do |second|
+    analyzer.compare(first, second)
+  end
+end
